bb3dc9744e5dd9d021dc9cde705fd1d08e5b8371
Etim Israel <israeletim4@gmail.com>
2025-06-08 04:41:54 +0100

    changed app name

--- ios/bmiCalculatorTests/bmiCalculatorTests.m
+++ ios/bmiCalculatorTests/bmiCalculatorTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface bmiCalculatorTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation bmiCalculatorTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
